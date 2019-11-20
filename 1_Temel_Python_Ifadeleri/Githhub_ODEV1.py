0130099cb272714f0858313209170078c7ccd0d5
aybarsorm <57523619+aybarsorm@users.noreply.github.com>
2019-11-20 14:47:31 +0300

    Update Githhub_ODEV1.py

--- 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
+++ 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
@@ -14,16 +14,33 @@ Listeler:
     b-) Daha sonra oluşturduğunuz bu listeye "üniversite"den sonra "Bursa" kelimesini ekleyiniz.
     c-) En sondaki kelimeyi bu listeden siliniz.
     d-)Listedeki Kelimleri sondan başa doğru sıralayınız.
+#a
+liste = ["Uludag", "Üniversitesi", "Yapay", "Zeka", "Topluluğu"]
+print(liste.index("Yapay"))
 
-    
+#b
+liste.insert(2,"Bursa")
+
+#c
+liste.pop()
+
+#d
+liste[::-1]
     
 Set:
 4-) "lorem ipsum dolor sit amet ipsum dolor dolor sit amet  " 
     a-)cümlesindeki her harfin bir kere yazdığı kümeyi oluşturunuz.
     b-)bu cümlenin uzunluğunu ekrana yazdırınız.
+#a
+yazi2 = "lorem ipsum dolor sit amet ipsum dolor dolor sit amet  "
 
+kume = tuple(yazi)
+
+#b
+print(len(yazi))
 
   
  Dict:
 5-)Bir sözlük oluşturup 4 farklı bilgiyi bu sözlükte depolayınız.(indexleri 1,2,3,4 olacak şekilde)
 
+sozluk = {"car" : "araba", "snow" : "kar", "banana" : "muz", "fly" : "sinek"}
