3e2e5136a022f8ca58bc65403bbd3d8f35ec41fc
aybarsorm <57523619+aybarsorm@users.noreply.github.com>
2019-11-21 22:51:51 +0300

    Update Githhub_ODEV1.py

--- 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
+++ 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
@@ -1,11 +1,16 @@
 1-)Kullanıcıdan bir kelime alıp bu kelimeyi bir değişkene aktardıktan sonra yan yana 10 kere yazdırınız.
 
+kelime = input("kelime yazınız")
 
+print(kelime*10)
 
 
 2-)Kullanıcıdan aldığınız iki sayının çarpımı ile bölümlerinin arasındaki fark nedir?
 
+x = int(input("1. tam sayıyı giriniz: "))
+y = int(input("2. tam sayıyı giriniz: "))
 
+print((x*y)-(x/y))
 
 
 
